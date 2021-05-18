85da76cae2737355557797a7898b86103eceaeb6
pisun <pisun.c.l@gmail.com>
2021-05-18 19:23:29 -0400

    first commit

---
+++ typing.js
@@ -0,0 +1,18 @@
+let quotes_array = [
+    "Push yourself, because no one else is going to do it for you.",
+    "Failure is the condiment that gives success its flavor.",
+    "Wake up with determination. Go to bed with satisfaction.",
+    "It's going to be hard, but hard does not mean impossible.",
+    "Learning never exhausts the mind.",
+    "The only way to do great work is to love what you do."
+];
+
+let LIMIT = 60;
+
+let timer_text = document.getElementById('timer_text')
+let wpm_text = document.getElementById('wpm_text')
+let error_text =  document.getElementById('error_text')
+let reader =  document.getElementById('reader')
+let input_box =  document.getElementById('input_box')
+
+
